17b6012d7bd3a268d1074016b012ad3ed2e42afd
chris2450 <rulers321@gmail.com>
2018-03-06 07:02:36 -0800

    added a get request for information from db

--- src/app/contact.service.ts
+++ src/app/contact.service.ts
@@ -1,8 +1,15 @@
-import { Injectable } from '@angular/core';
+// import { Injectable } from '@angular/core';
+// import { Http, Headers } from '@angular/http';
+// import 'rxjs/add/operator/map';
 
-@Injectable()
-export class ContactService {
+// @Injectable()
+// export class ContactService {
 
-  constructor() { }
+//   constructor(private http: Http) { }
 
-}
+//   GetContacts() {
+//     console.log(this.http.get('http://localhost:3000/api/contacts')
+//   .map(res => res.json()));
+//   }
+
+// }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+
+
+  getContacts() {
+    this.http.get('http://localhost:3000/api/contacts').subscribe(response => console.log(response));
+  }
+
 
   ngOnInit() {
+    console.log('hi!');
   }
 
 }
